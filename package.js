e2bca57cfe8284d4e36b685c988c2ade78273a94
jordbutler <91701088+jordbutler@users.noreply.github.com>
2023-04-07 18:09:19 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "jordbutlerCMPS415ProjectPhase1",
+  "version": "1.0.0",
+  "description": "CMPS415ProjectPhase1",
+  "main": "main.js",
+  "repository": "https://github.com/jordbutler/CMPS415ProjectPhase1",
+  "author": "jordbutler",
+  "license": "MIT",
+  "private": false,
+  "scripts": {
+    "start": "node main.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": ">=14"
+  }
+}
